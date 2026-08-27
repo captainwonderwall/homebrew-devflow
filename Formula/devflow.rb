7a41f4cc474b51da8c8672f262fad47d0735fc54
CaptainDaddy <4224955+captainwonderwall@users.noreply.github.com>
2026-08-26 21:43:04 -0400

    chore: update devflow-sdk resource to v1.1.0 in homebrew formula

--- Formula/devflow.rb
+++ Formula/devflow.rb
@@ -11,8 +11,8 @@ class Devflow < Formula
 
   # TODO: update url and sha256 after releasing devflow-sdk/v1.1.0
   resource "devflow-sdk" do
-    url "https://github.com/captainwonderwall/devflow-platform/releases/download/devflow-sdk%2Fv1.0.2/devflow_sdk-1.0.2-py3-none-any.whl"
-    sha256 "7f3e6bcda313c3d3381a865206628122c3606ea93087da46e7a037fdec23ba04"
+    url "https://github.com/captainwonderwall/devflow-platform/releases/download/devflow-sdk%2Fv1.1.0/devflow_sdk-1.1.0-py3-none-any.whl"
+    sha256 "97b2be3eef6838624f105f66d2d07108c364a6c3b69136d3acce8ceca84435fa"
   end
 
   resource "questionary" do
